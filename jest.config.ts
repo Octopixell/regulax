b43e75e8946e976eca75f0da46bf7fa797ab4b1c
Marco Strijker <marco@cord.co>
2023-03-17 11:21:37 +0100

    test: use ts-jest instead of babel with preset-typescript

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,11 @@ import { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   testPathIgnorePatterns: ['dist'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
 };
 
 export default config;
